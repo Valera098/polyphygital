2481419ac78ac3a48586caf9defc7eb6dd2e2ef2
Valera098 <Valera098Main@mail.ru>
2023-06-08 08:09:28 +0300

    registration/autorization

--- polyphygital/newsblock/urls.py
+++ polyphygital/newsblock/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('', index, name = 'homepage'),
     path('news/', news, name = 'newspage'),
     path('news/post/<slug:post_slug>', show_post, name='post'),
+    path('register/', RegisterUser.as_view(), name = 'register'),
+    path('login/', LoginUser.as_view(), name = 'login'),
+    path('logout/', logout_user, name='logout'),
 ]
--- polyphygital/newsblock/views.py
+++ polyphygital/newsblock/views.py
@@ -1,7 +1,14 @@
+from django.contrib.auth import logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.views import LoginView
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
+from newsblock.forms import RegisterUserForm
 from newsblock.models import *
+from newsblock.utils import *
 
 
 def index(request):
@@ -27,6 +34,37 @@ def show_post(request, post_slug):
 
     return render(request, 'newsblock/post.html', context=context);
 
+# def login(request, ):
+#     return HttpResponse("Авторизация");
+
 def pageNotFound(request, exception):
     return HttpResponse('Ты находишься на <b>СТРАНИЦЕ ОШИБКИ</b>');
     # return render(request, 'newsblock/404.html', {'menu': menu, 'title': 'Упс...'})
+
+
+class RegisterUser(DataMixin, CreateView):
+    form_class = RegisterUserForm
+    template_name = 'newsblock/register.html'
+    success_url = reverse_lazy('login')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Регистрация")
+        return dict(list(context.items()) + list(c_def.items()))
+
+
+class LoginUser(DataMixin, LoginView):
+    form_class = AuthenticationForm
+    template_name = 'newsblock/login.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Авторизация")
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def get_success_url(self):
+        return reverse_lazy('homepage')
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
